fe44988b1be924c4bb230c05024424ae79156cdb
YakiFrog <Frog7352@icloud.com>
2025-04-12 15:34:04 +0900

    0.13.3 文字に白シャドー

--- renderer/components/AttendanceRanking.tsx
+++ renderer/components/AttendanceRanking.tsx
@@ -422,7 +422,7 @@ const AttendanceRanking: React.FC<AttendanceRankingProps> = ({ maxRanks = 5 }) =
             </Badge>
           </Box>
           
-          <Text fontWeight="bold" fontSize="xl" textShadow = "2px 0px 10px rgba(255, 255, 255, 0.9)">
+          <Text fontWeight="bold" fontSize="xl" textShadow = "0px 1px 10px rgba(255, 255, 255, 0.9)">
             {item.name}
           </Text>
           
